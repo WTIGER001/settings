33ab30e915e8a451b6a7b61a3fb8cdcd3e5aa359
John Bauer <John.bauer.iii@gmail.com>
2018-05-08 16:30:19 -0400

    JSON editing works

--- src/app/setting-editor/setting-editor.component.ts
+++ src/app/setting-editor/setting-editor.component.ts
@@ -11,7 +11,7 @@ export class SettingEditorComponent implements OnInit {
 
   data = {}
   schema = {}
-  layout = {}
+  layout = []
 
   @Input() setting: Preference
   @Input() spec: PreferenceDefinition
@@ -28,15 +28,24 @@ export class SettingEditorComponent implements OnInit {
 
   ngOnInit() {
     this.data = this.setting.value
-    // console.log(this.spec);
-    // console.log("schema : " + this.spec.schema)
-    let st: string = JSON.stringify(this.spec.schema)
-    console.log(this.sch);
+    // // convert the schema to a real object
+    // let st: string = JSON.stringify(this.spec.schema)
+    // let mySch = JSON.parse(st);
 
-    this.schema = this.sch
+    // console.log("Schema 1");
+    // this.schema = mySch
+    // // console.log(this.schema);
 
-    // this.schema = this.spec.schema
+    // console.log("Schema 2");
+    // this.schema = this.sch
+    // // console.log(this.schema);
+
     // this.layout = this.spec.layout
+
+    this.schema = JSON.parse(<string>this.spec.schema)
+
+    this.layout = JSON.parse(<string>this.spec.layout)
+
   }
 
   get code() {
@@ -62,13 +71,25 @@ export class SettingEditorComponent implements OnInit {
   }
 
   save(event, setting) {
-    console.log("SAVING DATA")
-    console.log(event)
+    // console.log("SAVING DATA")
+    // console.log(event)
     setting.data = event
   }
 
+  exampleJsonObject = {
+    "first_name": "Jane", "last_name": "Doe", "age": 25, "is_company": false,
+    "address": {
+      "street_1": "123 Main St.", "street_2": null,
+      "city": "Las Vegas", "state": "NV", "zip_code": "89123"
+    },
+    "phone_numbers": [
+      { "number": "702-123-4567", "type": "cell" },
+      { "number": "702-987-6543", "type": "work" }
+    ],
+    "notes": ""
+  };
+
   sch = {
-    "$schema": "http://json-schema.org/draft-06/schema#",
     "type": "object",
     "properties": {
       "first_name": {
@@ -135,4 +156,92 @@ export class SettingEditorComponent implements OnInit {
       "last_name"
     ]
   }
+
+  lay = [
+    {
+      "type": "flex",
+      "flex-flow": "row wrap",
+      "items": [
+        "first_name",
+        "last_name"
+      ]
+    },
+    {
+      "key": "address.street_1",
+      "title": "Address",
+      "placeholder": "Street"
+    },
+    {
+      "key": "address.street_2",
+      "notitle": true
+    },
+    {
+      "type": "div",
+      "display": "flex",
+      "flex-direction": "row",
+      "items": [
+        {
+          "key": "address.city",
+          "flex": "3 3 150px",
+          "notitle": true,
+          "placeholder": "City"
+        },
+        {
+          "key": "address.state",
+          "flex": "1 1 50px",
+          "notitle": true,
+          "placeholder": "State"
+        },
+        {
+          "key": "address.zip_code",
+          "flex": "2 2 100px",
+          "notitle": true,
+          "placeholder": "Zip Code"
+        }
+      ]
+    },
+    {
+      "key": "birthday",
+      "type": "date"
+    },
+    {
+      "key": "phone_numbers",
+      "type": "array",
+      "listItems": 3,
+      "items": [
+        {
+          "type": "div",
+          "displayFlex": true,
+          "flex-direction": "row",
+          "items": [
+            {
+              "key": "phone_numbers[].type",
+              "flex": "1 1 50px",
+              "notitle": true,
+              "placeholder": "Type"
+            },
+            {
+              "key": "phone_numbers[].number",
+              "flex": "4 4 200px",
+              "notitle": true,
+              "placeholder": "Phone Number"
+            }
+          ]
+        }
+      ]
+    },
+    {
+      "type": "section",
+      "title": "Notes",
+      "expandable": true,
+      "expanded": false,
+      "items": [
+        {
+          "key": "notes",
+          "type": "textarea",
+          "notitle": true
+        }
+      ]
+    }
+  ]
 }
